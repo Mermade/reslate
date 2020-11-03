b2ea8591a0d13b1d7058d531f349e7a05601abf3
Mike Ralphson <mike.ralphson@gmail.com>
2020-11-03 08:32:40 +0000

    tests: add build test

--- reslate.js
+++ reslate.js
@@ -11,20 +11,20 @@ const sh = require('shelljs');
 const env = Object.assign({}, process.env, {
   'SLATEDIR': path.relative(__dirname, process.cwd()),
   'NODE_PATH': path.resolve(__dirname, './node_modules')
- });
+});
 
 if (process.argv[2] === 'init') {
   // TODO test for empty directory except dotfiles?
   sh.cp('-R', __dirname+'/source/*', '.');
 }
 else if (process.argv[2] === 'build') {
-  cp.spawn('npm',[ 'run', 'build.local' ], { stdio: 'inherit', cwd: __dirname, env: env }, function(err, output) {
+  cp.spawn('npm',[ 'run', 'build.local' ], { stdio: 'inherit', cwd: __dirname, env }, function(err, output) {
     if (err) console.warn(err);
     console.log(output);
   });
 }
 else if (process.argv[2] === 'serve') {
-  cp.spawn('npm',[ 'run', 'serve.local' ], { stdio: 'inherit', cwd: __dirname, env: env }, function(err, output) {
+  cp.spawn('npm',[ 'run', 'serve.local' ], { stdio: 'inherit', cwd: __dirname, env }, function(err, output) {
     if (err) console.warn(err);
     console.log(output);
   });
---
+++ test/main.test.js
@@ -0,0 +1,26 @@
+const assert = require('assert');
+const cp = require('child_process');
+const fs = require('fs');
+const path = require('path');
+
+// path.relative is to work around https://github.com/11ty/eleventy/issues/1100
+const env = Object.assign({}, process.env, {
+  'SLATEDIR': './source',
+  'NODE_PATH': path.resolve(__dirname, '../node_modules')
+});
+
+function compare(a, b) {
+  const as = fs.readFileSync(a, 'utf8');
+  const bs = fs.readFileSync(b, 'utf8');
+  assert.equal(as, bs);
+}
+
+describe('reslate tests', () => {
+  it('build artefacts should match golden', () => {
+    cp.spawnSync('npm',[ 'run', 'build.local' ], { stdio: 'inherit', cwd: __dirname, env });
+    compare('./_site/index.html','test/golden/index.html');
+    compare('./_site/slate/css/print.css','test/golden/print.css');
+    compare('./_site/slate/css/screen.css','test/golden/screen.css');
+  }).timeout(10000);
+});
+
